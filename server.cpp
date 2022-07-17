6089d8a427eeee6301659f2d3157ba85d5fd90d1
Gildo Gutemberg <62493940+gildex174@users.noreply.github.com>
2022-07-17 12:28:37 -0300

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -4,12 +4,52 @@
 #include <tuple>
 #include <algorithm>
 
-#define SERVER_PORT 8192
+#define SERVER_PORT 8193
 
 using namespace std;
 
-Socket* server = new Socket(SERVER_PORT);
+Socket* server = new Socket("0.0.0.0", SERVER_PORT);
+int number_of_clients = 0;
 
+void partitionateMessage(Socket * server, string message) {
+    for(int i = 0 ; i < message.size() ; i += MAX_BUFFER_SIZE-1){
+        server->Broadcast(message.substr(i, min(MAX_BUFFER_SIZE-1, (int)message.size()-i+1)));
+    }
+}
+
+void *read_thread(void* arg){
+    int failed = 0;
+    bool quit = false;
+    int client = *((int*)arg);
+    int client_id = ++number_of_clients;
+
+    string message = "client#" + to_string(client_id) + " connected to the server";
+    cout << message << endl;
+    partitionateMessage(server, message);
+
+    while (!quit) {
+        string original_message = server->Read(client);
+        message = "client#" + to_string(client_id) + ":" + original_message;
+        cout << message << endl;
+
+        if (original_message == "/quit") {
+            message = "client#" + to_string(client_id) + " left the server";
+            cout << message << endl;
+            partitionateMessage(server, message);
+
+            quit = true;
+            server->Disconnect(client);
+        }
+        else if(original_message == "/ping"){
+            server->Write("Server: pong", client);
+
+        } else {
+            partitionateMessage(server, message);
+        }
+        message = "";
+    }
+    pthread_exit(NULL);
+}
 
 int main(int argc, char* argv[]){
 
@@ -17,32 +57,26 @@ int main(int argc, char* argv[]){
 
     cout << "Server Connected :)\n";
 
-    server->Listen();
+    while(1) {
+        server->Listen();
 
-    string ip;
-    int client;
-    tie(client, ip) = server->Accept();
+        string ip;
+        int client;
+        tie(client, ip) = server->Accept();
 
-    server->Broadcast("Sending from server to client!\n");
+        server->Write("You are connected!\n", client);
 
-    int failed = 0;
-    bool quit = false;
+        pthread_t tid;
+        pthread_attr_t thread_attr;
+    
+        pthread_attr_init(&thread_attr);
+        pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
 
-    while (!quit) {
-        cout << "aqui\n";
-        string message = server->Read(client);
-        cout << message << endl;
-
-        if (message == "/quit") {
-            cout << "Client left the server" << "\n";
-            quit = true;
-
-        } else {
-            for(int i = 0 ; i < message.size() ; i += MAX_BUFFER_SIZE-1){
-                server->Broadcast(message.substr(i, min(MAX_BUFFER_SIZE-1, (int)message.size()-i+1)));
-            }
+        // Create thread to read messages from this new client
+        if (pthread_create(&tid, &thread_attr, read_thread, &client) != 0) {
+            cout << "Failed to create thread to send message" << endl;
         }
-        message = "";
     }
-    cout << "saiu\n";
+
+    server->Disconnect();
 }
