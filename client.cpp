485fab244ffb7667e2fdb4ee2e4b617788503a9b
Gildo Gutemberg <62493940+gildex174@users.noreply.github.com>
2022-07-17 12:31:48 -0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,15 +1,22 @@
 #include "socket.h"
 #include <pthread.h>
+#include<csignal>
+//#include<stdio.h>
 
 #include <iostream>
 
 using namespace std;
 
-#define SERVER_PORT 8192
-#define SERVER_NAME "127.0.0.1"
+#define SERVER_PORT 8193
 
-Socket* client = new Socket(LOCALHOST_PORT);
+Socket* client;
 bool quit = false;
+
+void Sigint_handler(int sig_num) {
+    signal(SIGINT, Sigint_handler);
+    cout << "\nCannot quit using crtl+c\n";
+    fflush(stdout);
+}
 
 void* send_thread(void* arg) {    
     int failed = 0;
@@ -17,92 +24,118 @@ void* send_thread(void* arg) {
     string message = "";    
 
     while (!quit) {
-        do {
-            cout << "To Server: ";
-            getline(cin, message, '\n');
-        } while (message.size() == 0); 
+        //getline(cin, message, '\n');
 
-        cout << message << endl;
+        if(getline(cin, message, '\n')){
 
-        if (message == "/quit") {
-            cout << "Client left the server" << "\n";
-            quit = true;
 
-        } else {
+            if (message == "/quit")
+                quit = true;
+                //break;
+
             for(int i = 0 ; i < message.size() ; i += MAX_BUFFER_SIZE-1){
                 client->Write(message.substr(i, min(MAX_BUFFER_SIZE-1, (int)message.size()-i+1)));
             }
+
+            message = "";
+        }
+        else{
+            quit = true;
+            message = "/quit";
+
+            for(int i = 0 ; i < message.size() ; i += MAX_BUFFER_SIZE-1){
+                client->Write(message.substr(i, min(MAX_BUFFER_SIZE-1, (int)message.size()-i+1)));
+            }
+
+            message = "";
         }
 
-        message = "";
+       
     }
-    cout << "saiu\n";
     pthread_exit(NULL);
 }
 
-void* receive_thread(void* arg) {    
+void* receive_thread(void* arg) {
     int failed = 0;
-    int sender = *((int*)arg);
+    string message = "";
 
     while (!quit) {
-        string message = client->Read(client->sockfd);
-        cout << message << endl;
-        message = "";
+        do {
+            message = client->Read(client->sockfd);
+            cout << message  << endl;
+
+            message = "";
+        } while(message.size() ==  MAX_BUFFER_SIZE-1 && !quit);
     }
-    cout << "saiu\n";
     pthread_exit(NULL);
 }
 
 int main(int argc, char* argv[]) {
 
-    char server_name[50];
-    for(int i=0;i<9;i++){
-        server_name[i] = SERVER_NAME[i];
+    signal(SIGINT, Sigint_handler);
+    
+    if (argc < 2) {
+        cout << "Provide your ip, please\n";
+        return 1; 
     }
 
-    client->Connect(server_name, SERVER_PORT);
+    string ip = argv[1];
     
+    //Alteração
+    string command = "";
+
+    cout << "Conecte-se ao servidor digitando /connect.\n";
+
+    while(command != "/connect"){
+        if(!getline(cin, command, '\n')) {
+            cout << "quitting" << endl;
+            exit(0);
+        }
+
+        else if(command == "/quit")
+            exit(0);
+
+    }
+    
+    client = new Socket(ip, LOCALHOST_PORT);
+    client->Connect(SERVER_PORT);
+
     string message = "";
 
-    // while (message != "/quit") {
-
-    //     /* Garante que todas as mensagens enviadas serão recebidas */        
-    //     // do {
-    //     message = socket->Read();
-
-    //     cout << "From server: " << message << "\n";
-    //     // } while (message.size() == Socket::buffer_size);
-
-    //     // if (message.size() == 0) break;        
-
-    //     /* Espera o usuário digitar uma mensagem válida para enviar */
-    //     // do {
-    //         cout << "To Server: ";
-    //         getline(cin, message, '\n');
-    //     // } while (message.size() == 0); 
-
-
-    //     if (message == "/quit") break;
-
-    //     /*Divide mensagens com mais de "buffer_size" caracteres e as envia em sequência*/
-    //     // for (int i = 0; (unsigned int)i < message.size(); i += Socket::buffer_size) {
-    //     socket->Write(message);
-    //         // socket->Check();
-    //     // }
-    // }
-
     pthread_t tid_send, tid_receive;
+    pthread_attr_t thread_attr;
+    void * status;
 
-    if (pthread_create(&tid_send, NULL, send_thread, NULL) != 0) {
+    pthread_attr_init(&thread_attr);
+    pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
+
+    //Create thread to watch messages sent by this client
+    if (pthread_create(&tid_send, &thread_attr, send_thread, NULL) != 0) {
         cout << "Failed to create thread to send message" << endl;
         return 1;
     }
-    if (pthread_create(&tid_receive, NULL, receive_thread, NULL) != 0) {
+
+    //Create thread to watch messages received by this client
+    if (pthread_create(&tid_receive, &thread_attr, receive_thread, NULL) != 0) {
         cout << "Failed to create thread to receive message" << endl;
         return 1;
     }
 
+    pthread_attr_destroy(&thread_attr);
+
+    if(pthread_join(tid_send, &status)) {
+        cout << "Failed to join send thread" << endl;
+        exit(1);
+    }
+    pthread_cancel(tid_receive);
+    if(pthread_join(tid_receive, &status)) {
+        cout << "Failed to join receive thread" << endl;
+        exit(1);
+    }
+
     client->Disconnect();
+
+    cout << "Client has disconnected from the server" << endl;
     
     delete client;
     return 0;
